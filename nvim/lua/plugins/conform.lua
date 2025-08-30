2d1fa3d39ca7b7c415488a97974ecb4c1facfe4a
NemanjaTomic57 <nemanja.tomic@ik.me>
2025-08-30 16:16:07 +0200

    format on save

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -46,11 +46,11 @@ return {
 				yaml = { "prettier" },
 				markdown = { "prettier" },
 			},
-			-- format_on_save = {
-			-- 	lsp_fallback = true,
-			-- 	async = false,
-			-- 	timeout_ms = 1000,
-			-- },
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			},
 		})
 
 		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
