6f54e01cc81e72f352818f0da52533b0291ceec8
NemanjatTomic57 <nemanja.tomic@ik.me>
2025-11-26 19:18:27 +0100

    update several configurations

--- desktop/.config/nvim/lua/plugins/theme.lua
+++ desktop/.config/nvim/lua/plugins/theme.lua
@@ -1,22 +1,12 @@
 return {
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        priority = 1000,
-        opts = {
-            style = "night",
-        },
-        config = function()
-            vim.cmd.colorscheme 'tokyonight'
-        end,
-    },
-    -- {
-    --     'catppuccin/nvim',
-    --     name = 'mocha',
-    --     lazy = false,
-    --     priority = 1000,
-    --     config = function()
-    --         vim.cmd.colorscheme 'catppuccin-macchiato'
-    --     end,
-    -- },
+	"folke/tokyonight.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {
+		style = "night",
+	},
+	config = function(_, opts)
+		require("tokyonight").setup(opts)
+		vim.cmd("colorscheme tokyonight")
+	end,
 }
--- desktop/bash/bashrc.sh
+++ desktop/bash/bashrc.sh
@@ -10,9 +10,9 @@ PS1='[\u@\h \W]\$ '
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 alias open='xdg-open'
-alias sc='cd ~/Desktop/dotfiles/scripts/shell/bin/'
-alias ws='cd ~/Desktop/workspace/'
-alias kd="~/Desktop/kDrive.AppImage"
+alias sc='cd ~/Documents/workspace/dotfiles/scripts/shell/bin/'
+alias ws='cd ~/Documents/workspace/'
+alias df='cd ~/Documents/workspace/dotfiles/desktop/'
 
 # Shell scripts 
 export PATH="$HOME/Desktop/dotfiles/scripts/shell/bin/:$PATH"
