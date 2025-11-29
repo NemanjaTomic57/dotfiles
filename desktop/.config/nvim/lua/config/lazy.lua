3e24113c7c2fbd730ebf65f1ca51d46f28111252
NemanjatTomic57 <nemanja.tomic@ik.me>
2025-11-29 18:05:32 +0100

    configure kitty, edit nvim colorscheme, tweak nvim lazy.lua

--- desktop/.config/nvim/lua/config/lazy.lua
+++ desktop/.config/nvim/lua/config/lazy.lua
@@ -1,23 +1,27 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"--branch=stable", -- latest stable release
+		lazyrepo,
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    spec = {
-        { import = "plugins" },
-        { import = "plugins.lsp" },
-    },
-    checker = { enabled = true },
-    rocks = {
-        enabled = false,
-    },
+	spec = {
+		{ import = "plugins" },
+		{ import = "plugins.lsp" },
+	},
+	defaults = {
+		lazy = false,
+	},
+	checker = { enabled = true },
+	rocks = {
+		enabled = false,
+	},
 })
