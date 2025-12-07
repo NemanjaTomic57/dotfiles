3a2eca84c096ad5e35a8ca41c98ee681c0d5a781
NemanjatTomic57 <nemanja.tomic@ik.me>
2025-12-07 19:03:11 +0100

    update terminal theme and font

--- desktop/bash/bashrc.sh
+++ desktop/bash/bashrc.sh
@@ -2,6 +2,9 @@
 # ~/.bashrc
 #
 
+# NOTE:
+# OSH_THEME="brainy"
+
 # If not running interactively, don't do anything
 [[ $- != *i* ]] && return
 
@@ -12,7 +15,13 @@ alias grep='grep --color=auto'
 alias open='xdg-open'
 alias sc='cd ~/Documents/workspace/dotfiles/scripts/shell/bin/'
 alias ws='cd ~/Documents/workspace/'
-alias df='cd ~/Documents/workspace/dotfiles/desktop/'
+alias dotfiles='cd ~/Documents/workspace/dotfiles/desktop/'
+alias sourcebash='source /home/nemo/Documents/workspace/dotfiles/desktop/bash/bashrc.sh'
 
 # Shell scripts 
 export PATH="$HOME/Desktop/dotfiles/scripts/shell/bin/:$PATH"
+
+# pyenv for managing non-system python versions
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init - bash)"
